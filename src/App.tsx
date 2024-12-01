17e2daa8afec1111f842dc09e408723f36b39f6d
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-01 00:09:27 -0300

    basic form css set up

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 export default function App (){
     return (
             <section className="
-             w-screen h-screen grid justify-center">
-                <div className="grid grid-flow-col gap-6 w-6/12 h-1/2 items-center justify-center">
-                    <form className="flex items-start gap-6 flex-col">
-                        <input className="" type="text" id="password" name="password" placeholder="Insert password."/>
-                        <input type="submit" name="submit" className="submit" value="Login"/>
+             w-screen h-screen flex justify-center items-center">
+                <div className="shadow-xl flex flex-col w-1/2 h-1/2 items-center justify-center content-stretch bg-gray-100">
+                    <form className="flex items-start gap-6 flex-col rounded-b bg-transparent">
+                        <input className="p-2" autoComplete="off" type="text" id="password" name="password" placeholder="Insert password."/>
+                        <input type="submit" name="submit" className="hover:bg-blue-400 submit bg-primary p-2" value="Login"/>
                     </form>
                 </div>
             </section>
