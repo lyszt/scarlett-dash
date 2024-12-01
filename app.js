ae9fce4b345ae06c298138aa76eb7363966a499b
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-01 00:40:12 -0300

    setting up google o-auth

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import './App.css';
-
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const express = require('express');
 const path = require('path');
 
@@ -22,3 +29,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 })
+
+// Auth
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+const userSchema = new mongoose.Schema ({
+
+    googleId: String
+});
+userSchema.plugin(findOrCreate);const User = new mongoose.model("User", userSchema);
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/callback/url",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ export default function App (){
             <section className="
              w-screen h-screen flex justify-center items-center">
                 <div className="shadow-xl flex flex-col w-1/2 h-1/2 items-center justify-center content-stretch bg-gray-100">
-                    <form className="flex items-start gap-6 flex-col rounded-b bg-transparent">
-                        <input className="p-2" autoComplete="off" type="text" id="password" name="password" placeholder="Insert password."/>
-                        <input type="submit" name="submit" className="hover:bg-blue-400 submit bg-primary p-2" value="Login"/>
-                    </form>
+                    <div className="flex items-start gap-6 flex-col rounded-b bg-transparent">
+                        <a href='/auth' role="button" className="hover:bg-blue-400 bg-gray-300 bg-gradient-to-r p-3" value="Sign in with Google"/>
+                    </div>
                 </div>
             </section>
     );
