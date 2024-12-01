91383d935fe94690dbeb1cebd6b9815dbca6a150
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-12-01 00:44:54 -0300

    setting up google o-auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ export default function App (){
              w-screen h-screen flex justify-center items-center">
                 <div className="shadow-xl flex flex-col w-1/2 h-1/2 items-center justify-center content-stretch bg-gray-100">
                     <div className="flex items-start gap-6 flex-col rounded-b bg-transparent">
-                        <a href='/auth' role="button" className="hover:bg-blue-400 bg-gray-300 bg-gradient-to-r p-3" value="Sign in with Google"/>
+                        <a href='/auth' role="button" className="p-3 hover:no-underline hover:bg-blue-400 bg-gray-300 bg-gradient-to-r p-3/">
+                        Sign in with Google
+                        </a>
                     </div>
                 </div>
             </section>
