fcbf5cf8d81b423aae0027db713f3e98f29be998
lyszt <kalliddel@gmail.com>
2024-12-03 21:55:47 -0300

    new visual for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ export default function App (){
             <section className="
              w-screen h-screen flex justify-center items-center m-o max-w-full">
                 <div className="border-solid border-gray-200 border bg-white flex flex-col w-2/6 h-3/4 items-center justify-center content-stretch bg-gray-100 rounded-3xl">
-                    <form className="w-1/2 flex items-start gap-6 flex-col bg-transparent">
-                        <label for="password" className="bg-transparent text-left text-gray-500">Password:</label>
+                    <form className="w-1/2 flex items-center gap-6 flex-col bg-transparent">
+                        <img className="bg-transparent w-5/6 saturate-0 invert" src="/src/assets/crimsonanimation.gif"></img>
+                        <label for="password" className="bg-transparent text-left m-0 text-gray-500 m-10 ml-7">Password:</label>
                         <input type="text" name="password" className='p-3' placeholder="Insert your password."></input>
                         <input type="submit" name="submit" value="Sign-in" className=" p-3 hover:no-underline hover:bg-red-400 bg-gray-300 bg-gradient-to-r p-3/"></input>
                     </form>
