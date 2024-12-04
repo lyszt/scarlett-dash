ca06f4251f0e5f682ef78b482c45ad1eacbc2be6
lyszt <kalliddel@gmail.com>
2024-12-03 21:19:08 -0300

    removing google oauth for practice

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App (){
                 <div className="shadow-xl flex flex-col w-1/2 h-1/2 items-center justify-center content-stretch bg-gray-100">
                     <div className="flex items-start gap-6 flex-col rounded-b bg-transparent">
                         <a href='/auth' role="button" className="p-3 hover:no-underline hover:bg-blue-400 bg-gray-300 bg-gradient-to-r p-3/">
-                        Sign in with Google
+                        Sign in
                         </a>
                     </div>
                 </div>
