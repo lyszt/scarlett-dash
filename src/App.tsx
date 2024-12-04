08bf8f77ae9faf50d81208d020ae4595d6be95eb
lyszt <kalliddel@gmail.com>
2024-12-03 21:47:02 -0300

    Login screen simple design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 export default function App (){
     return (
             <section className="
-             w-screen h-screen flex justify-center items-center">
-                <div className="shadow-xl flex flex-col w-1/2 h-1/2 items-center justify-center content-stretch bg-gray-100">
-                    <div className="flex items-start gap-6 flex-col rounded-b bg-transparent">
-                        <a href='/auth' role="button" className="p-3 hover:no-underline hover:bg-blue-400 bg-gray-300 bg-gradient-to-r p-3/">
-                        Sign in
-                        </a>
-                    </div>
+             w-screen h-screen flex justify-center items-center m-o max-w-full">
+                <div className="border-solid border-gray-200 border bg-white flex flex-col w-2/6 h-3/4 items-center justify-center content-stretch bg-gray-100 rounded-3xl">
+                    <form className="w-1/2 flex items-start gap-6 flex-col bg-transparent">
+                        <label for="password" className="bg-transparent text-left text-gray-500">Password:</label>
+                        <input type="text" name="password" className='p-3' placeholder="Insert your password."></input>
+                        <input type="submit" name="submit" value="Sign-in" className=" p-3 hover:no-underline hover:bg-red-400 bg-gray-300 bg-gradient-to-r p-3/"></input>
+                    </form>
                 </div>
             </section>
     );
