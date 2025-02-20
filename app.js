a782254abe144ee172ff8cfd8ac9147a61b8d60c
lyszt <kalliddel@gmail.com>
2025-02-20 10:19:27 -0300

    setting for production

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.get('/auth', isAuthenticated, (req, res) => {
     res.status(200).send();
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0'() => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}`);
 });
 
